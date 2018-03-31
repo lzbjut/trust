2010b4d093b0f57d86c481120238cb2c87a6734d
lz <18519020056@139.com>
2018-03-31 21:03:42 +0800

    Rejected commit: Default

--- src/main/java/util/test/Main.java
+++ src/main/java/util/test/Main.java
@@ -1,7 +1,5 @@
 package util.test;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -13,53 +11,72 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n= sc.nextInt();
-        int m=sc.nextInt();
-        HashMap<Long,Long> pay=new HashMap<>();
-        long[] Pay=new long[n];
-        for(int i=0;i<n;i++){
-            long jobDiff=sc.nextLong();
-            long pzay=sc.nextLong();
-            pay.put(pzay,jobDiff);
-            Pay[i]=pzay;
+        int all=sc.nextInt();
+        for(int i=0;i<all;i++){
+            long [] list=new long[3];
+            list[0]=sc.nextLong();
+            list[1]=sc.nextLong();
+            list[2]=sc.nextLong();
+            System.out.println(solve(list));
         }
-        Arrays.sort(Pay);
-        for(int j=0;j<m;j++){
-            long ar=sc.nextLong();
-            for(int z=n-1;z>=0;z--){
-                if(pay.get(Pay[z])<=ar){
-                    System.out.println(Pay[z]);
-                    break;
+    }
+    static long solve(long[] list){
+        long sum=0;
+       while(true){
+           sort(list);
+           if(list[0]==0&&list[1]==0){
+               break;
+           }
+           if(list[0]==list[1]&&list[1]==list[2]){
+               sum+=list[0];
+               break;
+           }
+           else{
+               if(list[0]==0){
+                    if(list[1]*2<list[2]){
+                        sum+=list[0];
+                    }
+                    else{
+                        sum+=(list[0]+list[2])/3;
+                    }
+               }
+               else{
+                   long temp=(list[2]-list[1])/2+1;
+                   if(temp>=list[0]){
+                       list[2]-=list[0]*2;
+                       sum+=list[0];
+                       list[0]=0;
+
+                   }
+                   else{
+                       list[0]-=temp;
+                       list[2]-=temp*2;
+                       sum+=temp;
+                   }
+               }
+           }
+
+       }
+        return sum;
+
+
+
+    }
+    static void sort(long[] list){
+        for(int j=0;j<2;j++){
+            for(int i=0;i<2;i++){
+                if(list[i]>list[i+1]){
+                    long temp=list[i];
+                    list[i]=list[i+1];
+                    list[i+1]=temp;
                 }
             }
         }
 
-
     }
 
-    static void sort(long[] list,long[] listB,int left,int right){
-        if(left>=right){
-            return;
-        }
-        long temp=list[left],tempB=listB[left];
-        int start=left,end=right;
-        while(start<end){
-            while(end>start&&list[end]>=temp){
-                end--;
-            }
-            list[start]=list[end];
-            listB[start]=listB[end];
-            while(start<end&&list[start]<=temp){
-                start++;
-            }
-            list[end]=list[start];
-            listB[end]=listB[start];
-        }
-        list[start]=temp;
-        listB[start]=tempB;
-        sort(list,listB,left,start-1);
-        sort(list,listB,start+1,right);
-    }
+
+
 
 
 
